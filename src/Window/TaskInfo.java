6bd7eb65a0f43d4cb0174746daa90cc0f84f41f6
irinaprihodko <irinaprihodkoirina@gmail.com>
2019-05-05 00:12:57 +0300

    Create TaskInfo.java

---
+++ src/Window/TaskInfo.java
@@ -0,0 +1,13 @@
+package Window;
+
+public class TaskInfo {
+   public int numberOfTask;
+   public String description;
+   public String solution;
+
+    public TaskInfo(int numberOfTask, String description, String solution) {
+        this.numberOfTask = numberOfTask;
+        this.description = description;
+        this.solution = solution;
+    }
+}
